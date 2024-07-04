24f59efaa8754395cd17dc4f396cf00eca736ca6
RaymonIdle <variluoxx@gmail.com>
2024-07-03 18:55:26 -0700

    ds

--- XRP-Basics/src/main/java/frc/robot/subsystems/Drivetrain.java
+++ XRP-Basics/src/main/java/frc/robot/subsystems/Drivetrain.java
@@ -4,6 +4,7 @@
 
 package frc.robot.subsystems;
 
+import edu.wpi.first.wpilibj.Encoder;
 import edu.wpi.first.wpilibj.xrp.XRPMotor;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
@@ -12,6 +13,8 @@ public class Drivetrain extends SubsystemBase {
 
   private final XRPMotor leftMotor = new XRPMotor(0);
   private final XRPMotor rightMotor = new XRPMotor(1);
+  private final Encoder leftEncoder = new Encoder(4,5);
+  private final Encoder rightEncoder = new Encoder(6,7);
 
   public Drivetrain() {
     rightMotor.setInverted(true);
@@ -22,6 +25,23 @@ public class Drivetrain extends SubsystemBase {
     double right = fwd - turn;
     leftMotor.set(left);
     rightMotor.set(right);
+  }
+
+  public void resetEncoders(){
+    leftEncoder.reset();
+    rightEncoder.reset();
+  }
+
+  public double getLeftDistanceInch(){
+    return leftEncoder.getDistance();
+  }
+
+  public double getRightDistanceInch(){
+    return rightEncoder.getDistance();
+  }
+
+  public double getAvgDistanceInch(){
+    return (getLeftDistanceInch() + getRightDistanceInch())/2;
   }
 
 
